ef4e6e9595f990f48852afae138f5698b75f6d02
Elio <eliobricenov@gmail.com>
2025-09-09 18:04:30 -0400

    style: format import statements and clean up whitespace in deployment scripts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.8.0;
 
-import {Permit3} from "../src/Permit3.sol";
-import {ERC7702TokenApprover} from "../src/modules/ERC7702TokenApprover.sol";
-import {CommonBase} from "forge-std/Base.sol";
-import {Script} from "forge-std/Script.sol";
-import {StdChains} from "forge-std/StdChains.sol";
-import {StdCheatsSafe} from "forge-std/StdCheats.sol";
-import {StdUtils} from "forge-std/StdUtils.sol";
-import {console} from "forge-std/console.sol";
+import { Permit3 } from "../src/Permit3.sol";
+import { ERC7702TokenApprover } from "../src/modules/ERC7702TokenApprover.sol";
+import { CommonBase } from "forge-std/Base.sol";
+import { Script } from "forge-std/Script.sol";
+import { StdChains } from "forge-std/StdChains.sol";
+import { StdCheatsSafe } from "forge-std/StdCheats.sol";
+import { StdUtils } from "forge-std/StdUtils.sol";
+import { console } from "forge-std/console.sol";
 
 contract Deploy is Script {
     address public constant create2Factory = 0xce0042B868300000d44A59004Da54A005ffdcf9f;
@@ -23,10 +23,7 @@ contract Deploy is Script {
         console.log("Permit3:", permit3);
 
         // Deploy ERC7702TokenApprover with Permit3 address
-        bytes memory erc7702Code = abi.encodePacked(
-            type(ERC7702TokenApprover).creationCode,
-            abi.encode(permit3)
-        );
+        bytes memory erc7702Code = abi.encodePacked(type(ERC7702TokenApprover).creationCode, abi.encode(permit3));
         address erc7702Approver = deploy(erc7702Code, keccak256(abi.encode(salt, "ERC7702")));
         console.log("ERC7702TokenApprover:", erc7702Approver);
 
